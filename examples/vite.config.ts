a225a556a8ef7456f9d6fe6880fc36e18b0e463b
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-04-09 19:37:06 +0300

    refactor: removing extra vite files

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@@': '/src/components',
+    },
+  },
   plugins: [react()],
 })
