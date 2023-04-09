09b261cf7e2d1e4ebeb567b169a316574dd41052
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-04-09 21:44:24 +0300

    feat: simple-chatbot example initiated

---
+++ examples/src/pages/SimpleChatbot/index.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Simple Chatbot',
+  route: 'simple-chatbot',
+}
+
+export default function SimpleChatbot() {
+  return (
+    <div>
+      <h1>SimpleChatbot</h1>
+    </div>
+  )
+}
