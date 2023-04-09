1d6aa4b4e7dcf7a74b6a25aa6bb613195f37a0d2
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-04-09 15:04:17 +0300

    docs: better comments for chat class

--- packages/chat/src/Chat.ts
+++ packages/chat/src/Chat.ts
@@ -5,19 +5,27 @@ import { TChatOptions, TMessage } from './types';
 import getLatestMessagesOfChat from './utils/getLatestMessagesOfChat';
 
 class Chat {
-  readonly instructions;
+  // System prompt of the chat
+  readonly instructions: string;
+  // Messages sent in the chat
   readonly messages: TMessage[] = [];
+  // Maximum messages allowed in a request
   readonly maxMessagesPerRequest = 10;
-
+  // Options for the chat
   options: TChatOptions = {
     model: 'gpt-3.5-turbo',
     max_tokens: 100,
   };
 
+  // Instance of the Usage class
   usage = new Usage(this);
+
+  // Instance of the User class
   user = new User(this);
+  // Instance of the Assistant class
   assistant = new Assistant(this);
 
+  // Constructor for the Chat class
   constructor(instructions: string, options: Partial<TChatOptions> = {}) {
     this.instructions = instructions;
     this.options = {
@@ -26,6 +34,7 @@ class Chat {
     };
   }
 
+  // Get the latest messages in the chat
   get latestMessages(): TMessage[] {
     return getLatestMessagesOfChat(this);
   }
