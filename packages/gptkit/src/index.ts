a56a61526e333e1a28331d94dfc65499eec08cfe
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-04-11 00:04:04 +0300

    docs: added a comment in a the main library

--- packages/gptkit/src/index.ts
+++ packages/gptkit/src/index.ts
@@ -1,2 +1,7 @@
+/*
+  This package is a convenience package that re-exports the core and chat, and
+  any other packages that are added to the GPTKit ecosystem. This is the
+  package that should be used by the end user to import the GPTKit library.
+*/
 export { setOpenAIKey } from '@gptkit/core'
 export { createChat } from '@gptkit/chat'
