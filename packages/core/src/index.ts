9e2d1ce82bb9eadc5f31db5eb2931fc6d13189ea
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2023-04-12 01:49:53 +0300

    feat: created an infrastrucutre for proxying the requests

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,17 +1,50 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-// Get a new instance of OpenAi with the given API key
-const getOpenAiInstance = (apiKey: string) => {
-  const configuration = new Configuration({
-    apiKey,
-  });
-  return new OpenAIApi(configuration);
+// Extend the OpenAIApi class with an index signature
+interface OpenAIApiWithIndexSignature extends OpenAIApi {
+  [key: string | symbol]: unknown;
 }
 
-// Initialize openai with an empty API key
-let openai = getOpenAiInstance('');
+class Core {
+  private configuration = new Configuration();
+  private _unproxiedOpenAI = new OpenAIApi(this.configuration) as OpenAIApiWithIndexSignature;
+  openai = new Proxy(this._unproxiedOpenAI, {
+    get: (target, prop) => {
+      const method = target[prop];
+      if (method && typeof method === 'function') {
+        return (...args: unknown[]) => {
+          this.onRequest(args);
+          const result = method.apply(target, args);
+          Promise.resolve(result).then(this.onResponse);
+          return result;
+        }
+      }
+    }
+  });
+
+  constructor(apiKey = '') {
+    if (apiKey) {
+      this.apiKey = apiKey;
+    }
+  }
+
+  set apiKey(apiKey: string) {
+    this.configuration.apiKey = apiKey;
+    this.configuration.baseOptions.headers['Authorization'] = `Bearer ${apiKey}`;
+  }
+
+  private async onRequest(request: unknown) {
+    console.log(request);
+  }
+
+  private async onResponse(result: unknown) {
+    console.log(result);
+  }
+}
+
+const core = new Core();
 
 // Set openai's API key
-export const setOpenAIKey = (apiKey: string) => openai = getOpenAiInstance(apiKey);
+export const setOpenAIKey = (apiKey: string) => core.apiKey = apiKey;
 
-export default openai;
+export default core.openai;
